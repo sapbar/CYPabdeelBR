2b1b6170c2f4116fa8de0d4c6320b56b84df1eee
sapbar <poyosaa@gmail.com>
2019-09-19 11:24:23 -0500

    se sube ejemplo 1.10

---
+++ lidro/ejemplo1_10.py
@@ -0,0 +1,9 @@
+print ("calculo del area y perimetro de un tectangulo de un rectangulo")
+alt = int(input("introduce la altura de un tectangulo:"))
+base= int(input("itroduce el ancho del rectangulo:"))
+per= (alt*2) + (base*2)
+are= alt * base
+print ("EL perimetro del testangulo es:")
+print (per)
+print ("La altura del rectangulo es: ")
+print(are)
