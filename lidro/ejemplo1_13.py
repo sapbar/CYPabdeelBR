9a4d14a8923636245371935a6fd45ce4472dde94
sapbar <poyosaa@gmail.com>
2019-09-20 01:24:11 -0500

    mas ejercicios del libro

---
+++ lidro/ejemplo1_13.py
@@ -0,0 +1,8 @@
+cal1=float(input("inserte calificacion uno :"))
+cal2=float(input("inserte calificacion dos :"))
+cal3=float(input("inserte calificacion tres :"))
+cal4=float(input("inserte calificacion cuatro :"))
+cal5=float(input("inserte calificacion cinco :"))
+prom= (cal1 + cal2 + cal3  + cal4 + cal5) / 5
+print(f"El promedio del alumno es : {prom}")
+
---
+++ lidro/ejemplo1_14.py
@@ -0,0 +1,5 @@
+num=int(input"DAME UN NMERO ENTERO POSITIVO :")
+CUA= num **2
+CUB= num **3
+print(f"el cuadrado del numero {num} es {cua} y el cubo vale {cub}")
+
